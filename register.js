bc43e1f064420d5205898ce5ca3dd54f92953a5e
supersegacd <ipetrov0402@gmail.com>
2024-08-13 23:00:05 +0400

    r

--- register.js
+++ register.js
@@ -1,45 +1,93 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-analytics.js";
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js";
+import {getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword} from "https://www.gstatic.com/firebasejs/10.11.1/firebase-auth.js";
+import{getFirestore, setDoc, doc} from "https://www.gstatic.com/firebasejs/10.11.1/firebase-firestore.js"
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyBou-JHfDHyBQwgqy_Q4PMyJroZ6HLxs9A",
-  authDomain: "framesoftware-fc1a2.firebaseapp.com",
-  databaseURL: "https://framesoftware-fc1a2-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "framesoftware-fc1a2",
-  storageBucket: "framesoftware-fc1a2.appspot.com",
-  messagingSenderId: "290009632449",
-  appId: "1:290009632449:web:fd945d5ec6a08b4b3e0e40",
-  measurementId: "G-N0K5M369RQ"
+  const firebaseConfig = {
+    apiKey: "AIzaSyBou-JHfDHyBQwgqy_Q4PMyJroZ6HLxs9A",
+    authDomain: "framesoftware-fc1a2.firebaseapp.com",
+    databaseURL: "https://framesoftware-fc1a2-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "framesoftware-fc1a2",
+    storageBucket: "framesoftware-fc1a2.appspot.com",
+    messagingSenderId: "290009632449",
+    appId: "1:290009632449:web:fd945d5ec6a08b4b3e0e40",
+    measurementId: "G-N0K5M369RQ"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
-const submit = document.getElementById('submit');
-submit.addEventListener("click", function (event) {
-  event.preventDefault()
+function showMessage(message, divId){
+   var messageDiv=document.getElementById(divId);
+   messageDiv.style.display="block";
+   messageDiv.innerHTML=message;
+   messageDiv.style.opacity=1;
+   setTimeout(function(){
+       messageDiv.style.opacity=0;
+   },5000);
+}
+const signUp=document.getElementById('submitSignUp');
+signUp.addEventListener('click', (event)=>{
+   event.preventDefault();
+   const email=document.getElementById('rEmail').value;
+   const password=document.getElementById('rPassword').value;
+   const firstName=document.getElementById('fName').value;
+   const lastName=document.getElementById('lName').value;
 
-  const email = document.getElementById('email').value;
-  const password = document.getElementById('password').value;
+   const auth=getAuth();
+   const db=getFirestore();
 
-  createUserWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-      // Signed up 
-      const user = userCredential.user;
-      alert("acc..")
-      // ...
-    })
-    .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      alert(errorMessage)
-      // ..
-    });
+   createUserWithEmailAndPassword(auth, email, password)
+   .then((userCredential)=>{
+       const user=userCredential.user;
+       const userData={
+           email: email,
+           firstName: firstName,
+           lastName:lastName
+       };
+       showMessage('Account Created Successfully', 'signUpMessage');
+       const docRef=doc(db, "users", user.uid);
+       setDoc(docRef,userData)
+       .then(()=>{
+           window.location.href='index.html';
+       })
+       .catch((error)=>{
+           console.error("error writing document", error);
 
+       });
+   })
+   .catch((error)=>{
+       const errorCode=error.code;
+       if(errorCode=='auth/email-already-in-use'){
+           showMessage('Email Address Already Exists !!!', 'signUpMessage');
+       }
+       else{
+           showMessage('unable to create User', 'signUpMessage');
+       }
+   })
+});
 
+const signIn=document.getElementById('submitSignIn');
+signIn.addEventListener('click', (event)=>{
+   event.preventDefault();
+   const email=document.getElementById('email').value;
+   const password=document.getElementById('password').value;
+   const auth=getAuth();
+
+   signInWithEmailAndPassword(auth, email,password)
+   .then((userCredential)=>{
+       showMessage('login is successful', 'signInMessage');
+       const user=userCredential.user;
+       localStorage.setItem('loggedInUserId', user.uid);
+       window.location.href='homepage.html';
+   })
+   .catch((error)=>{
+       const errorCode=error.code;
+       if(errorCode==='auth/invalid-credential'){
+           showMessage('Incorrect Email or Password', 'signInMessage');
+       }
+       else{
+           showMessage('Account does not Exist', 'signInMessage');
+       }
+   })
 })
